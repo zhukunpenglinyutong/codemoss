bc89daf1b00d779fa6dd277bba425287d6e90d03
zhukunpenglinyutong <270750933@qq.com>
2026-09-06 16:03:24 +0800

    v1.0.0 init

--- src-tauri/src/main.rs
+++
@@ -1,9 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    if let Err(err) = fix_path_env::fix() {
-        eprintln!("Failed to sync PATH from shell: {err}");
-    }
-    cc_gui_lib::run()
-}
